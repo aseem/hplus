c97e8aea0ff0a77283d087c563aa208c98381350
aseem <aseem@aseem-mac.local>
2014-10-31 18:07:08 -0400

    Created ios client application

---
+++ client/ios/hplus/hplus/AppDelegate.h
@@ -0,0 +1,25 @@
+//
+//  AppDelegate.h
+//  hplus
+//
+//  Created by aseem on 10/31/14.
+//  Copyright (c) 2014 Aseem. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import <CoreData/CoreData.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@property (readonly, strong, nonatomic) NSManagedObjectContext *managedObjectContext;
+@property (readonly, strong, nonatomic) NSManagedObjectModel *managedObjectModel;
+@property (readonly, strong, nonatomic) NSPersistentStoreCoordinator *persistentStoreCoordinator;
+
+- (void)saveContext;
+- (NSURL *)applicationDocumentsDirectory;
+
+
+@end
+
